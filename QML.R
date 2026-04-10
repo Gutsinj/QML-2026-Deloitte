4ad1c8ddff3ce284b056c9eaf6957d74ea35a327
ethanabelev <ethanabelev@gmail.com>
2026-04-10 16:20:09 -0400

    r

--- QML.R
+++ QML.R
@@ -2,9 +2,10 @@ library(TSA)
 
 setwd("/Users/ethanabelev/Documents/Deloitte QML/QML-2026-Deloitte")
 df <- read.csv("lat_lon_timeseries_2.csv")   # adjust filename/path
+df["prediction"] <- exp(df["prediction"]) - 1
 str(df)
 head(df)
 plot(df,type="l")
 
 df <- df["prediction"]
-pacf(df)
+#acf(df)
